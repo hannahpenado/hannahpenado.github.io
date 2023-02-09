017ac923c32d03aee9e75bb722e10095fabd9d40
hannahpenado <44887679+hannahpenado@users.noreply.github.com>
2023-02-09 12:13:25 -0700

    Add files via upload
    Signed-off-by: hannahpenado <44887679+hannahpenado@users.noreply.github.com>

---
+++ movie_gen.py
@@ -0,0 +1,36 @@
+## start backend knowledge of genres and movies through dictionary and
+## hash map for movie values 
+
+    ## first want to create basic dictionary with keys as genres and 
+    ## values as movies
+        ##movie genres ie romance, action, horror, comedy, scifi, and drama
+
+
+    ## want to map out the values for selected movies
+        ## need to map out the rating values of the movies
+
+        ## need to map out the time length of movies
+        
+        ## need to map out actors for each movie
+    
+    ## create a stack function that can sort through and pop listed values
+    ## and return the movie name
+ 
+## get input from user
+    ## get input for movie genre first letter
+        ## check if they meant that genre y/n
+
+    ## want to create a stack or a queue for the movie genres and scope 
+        ## then pop the top values for the movies
+
+    ## ask user if they want to look at another genre y/n
+        ## yes = loop through again for stacks and hasing out
+            ## count user genres looked at, if more than 6 genres seen ask 
+            ## and tell this
+                ## that's all the genres we have and ask if they'd like to 
+                ## see a genre again
+                
+
+        ## no = end program
+    
+## thank user and end program
