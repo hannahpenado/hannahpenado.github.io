aa9a65d47c6647a8d02fd350706183e93fe8963b
hannahpenado <44887679+hannahpenado@users.noreply.github.com>
2023-02-10 10:28:52 -0700

    Update movie_gen.py
    Just updating the outline of what the code should do before starting to code to make it easier and quicker
    
    Signed-off-by: hannahpenado <44887679+hannahpenado@users.noreply.github.com>

--- movie_gen.py
+++ movie_gen.py
@@ -1,21 +1,90 @@
-## start backend knowledge of genres and movies through dictionary and
+## create Node class
+
+## create Stack class
+
+## create HashMap class
+
+## create MaxHeaps class
+
+## create empty list genres
+
+## create list of dictionary genre = {movies{rating, stars, length},...}
+    ## Romance = {}
+        # Can't Buy Love, Love Actually, The Notebook
+    
+    ## Action = {}
+        ## Die Hard, Maverick, Tron: Legacy
+    
+    ## Horror = {} 
+        ## Scream, Halloween, Friday the 13th
+    
+    ## Comedy = {}
+        ## Spy, Teladega Nights, 21 Jump Street
+    
+    ## Scifi = {}
+        ## Star Wars, Avatar, Interstellar
+    
+    ## Drama = {}
+        ## Don't Worry Darling, Hidden Figures, 127 Hours
+    
+    ## append all into genre list
+
+## create heapsort function
+
+## start backend knowledge of genres and movies through dictionary
+## and using a stack of the movies for each genre to later sort through and
 ## hash map for movie values 
-
-    ## first want to create basic dictionary with keys as genres and 
-    ## values as movies
-        ##movie genres ie romance, action, horror, comedy, scifi, and drama
-
-
     ## want to map out the values for selected movies
         ## need to map out the rating values of the movies
-
-        ## need to map out the time length of movies
         
-        ## need to map out actors for each movie
+    ## romance_stack 
+        ## create stacks by lowest rated first to highest rated with heap sort function
+        ## and hashmaps
+        
+        ## sort_genre(genre, movie_list) 
+            ## list of genre movies movie_list = [rating[movie]]
     
-    ## create a stack function that can sort through and pop listed values
-    ## and return the movie name
- 
+    ## action_stack
+        ## create stacks by lowest rated first to highest rated with heap sort function
+        ## and hashmaps
+        
+        ## sort_genre(genre, movie_list) 
+            ## list of genre movies movie_list = [rating[movie]]
+    
+    ## horror_stack
+        ## create stacks by lowest rated first to highest rated with heap sort function
+        ## and hashmaps
+         
+        ## sort_genre(genre, movie_list) 
+            ## list of genre movies movie_list = [rating[movie]]
+       
+    ## comedy_stack
+        ## create stacks by lowest rated first to highest rated with heap sort function
+        ## and hashmaps
+        
+        ## sort_genre(genre, movie_list) 
+            ## list of genre movies movie_list = [rating[movie]]
+       
+    ## scifi_stack
+        ## create stacks by lowest rated first to highest rated with heap sort function
+        ## and hashmaps
+        
+        ## sort_genre(genre, movie_list) 
+            
+            ## list of genre movies movie_list = [rating[movie]]
+    
+    ## drama_stack
+        ## create stacks by lowest rated first to highest rated with heap sort function
+        ## and hashmaps
+        
+        ## sort_genre(genre, movie_list) 
+            ## list of genre movies movie_list = [rating[movie]]
+    
+    ## create a stack function that can go through and pop movie values
+    ## and return the movie info
+
+## Make title for fuction HANNAH'S FAVORITES MOVIE SUGGESTOR with a big star
+
 ## get input from user
     ## get input for movie genre first letter
         ## check if they meant that genre y/n
@@ -29,8 +98,7 @@
             ## and tell this
                 ## that's all the genres we have and ask if they'd like to 
                 ## see a genre again
-                
-
+ 
         ## no = end program
     
 ## thank user and end program
